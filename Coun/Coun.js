cf6e772a8fce6df91887e8463762c36dd505c9c3
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-10-31 15:46:26 +0600

    install & setup react native

---
+++ Coun/Coun.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Coun() {
+  return (
+    <View>
+      <Text>Nazmul Hasan</Text>
+    </View>
+  )
+}
